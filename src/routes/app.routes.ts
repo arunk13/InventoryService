a9fe24b2337dcb4b6770cd1edfbae8b610c156b4
arunk13 <arun.infy13@gmail.com>
2016-08-13 12:40:28 -0400

    testing CD changes

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -12,7 +12,7 @@ router.use(requestLogger);
 
 var getAllFoodItems: express.RequestHandler = (req: express.Request, res: express.Response) => {
     console.log("getAllFoodItems");
-    res.end(JSON.stringify("Lamb Curry"));
+    res.end(JSON.stringify("Paneer Tikka"));
 }
 
 var getAllFoodItemById: express.RequestHandler = (req: express.Request, res: express.Response) => {
