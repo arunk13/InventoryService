17f5ec236465f267eef5671e31ff1cb2a5a4ef84
Arun Kumar <arun.infy13@gmail.com>
2016-08-16 15:45:24 -0400

    Testing

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -12,7 +12,7 @@ router.use(requestLogger);
 
 var getAllFoodItems: express.RequestHandler = (req: express.Request, res: express.Response) => {
     console.log("getAllFoodItems");
-    res.end(JSON.stringify("Okra"));
+    res.end(JSON.stringify("Banana chips"));
 }
 
 var getAllFoodItemById: express.RequestHandler = (req: express.Request, res: express.Response) => {
